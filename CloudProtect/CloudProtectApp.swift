55ca5a6e5ba9a8dfde7f5e24516c6f4bc02f5ad8
JoshTheCodeGuy0508 <m9cpcnkhdd@privaterelay.appleid.com>
2026-03-07 23:03:27 -0500

    Initial Commit

---
+++ CloudProtect/CloudProtectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudProtectApp.swift
+//  CloudProtect
+//
+//  Created by Joshua Varnum on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CloudProtectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloudProtectTests/CloudProtectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CloudProtectTests.swift
+//  CloudProtectTests
+//
+//  Created by Joshua Varnum on 3/7/26.
+//
+
+import Testing
+@testable import CloudProtect
+
+struct CloudProtectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
